9673d0c33fd1c5db73cd83f7021f96c094fa7be5
Ruben Saucedo <senrique@microsoft.com>
2026-01-09 17:20:01 -0800

    ds 1

--- 21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists.js
@@ -2,7 +2,7 @@
 
 /**
  * input
- * Two sorted linked lists
+ * Two *sorted* linked lists
  * 
  * output
  * A merged sorted linked list
@@ -12,7 +12,16 @@
  * Output: [1,1,2,3,4,4]
  * 
  * Breakdown solution
+ *  Usually for merging problems, we 1. create a new DS or 2. override one
+ *  Given they are sorted we should be able to use 2 pointers solution
+ * 
+ *  for 1.
+ *      place the pointers in the head
+ *      do value comparison
  */
 
+import { ListNode } from './data-structures/linkedi-list-node'
+
 var mergeTwoLists = function(list1, list2) {
+
 }
